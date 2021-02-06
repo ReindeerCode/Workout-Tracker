40c07f6d630266d7e21c6192868fb841b029b830
MJRudolph <ReindeerCode@gmail.com>
2021-02-06 14:54:46 -0500

    added seed to start

--- models/workouts.js
+++ models/workouts.js
@@ -43,19 +43,19 @@ const WorkoutSchema = new Schema(
         },
       },
     ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
   }
-  // {
-  //   toJSON: {
-  //     virtuals: true,
-  //   },
-  // }
 );
 
-// WorkoutSchema.virtual("totalDuration").get(function () {
-//   return this.exercises.reduce((total, exercise) => {
-//     return total + exercise.duration;
-//   }, 0);
-// });
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 
-const Workout = mongoose.model("workouts", WorkoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
